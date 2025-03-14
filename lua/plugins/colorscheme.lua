4eeda2552e85914d7e89144ee32bc120fa5b1e34
guilherme <devguilhermeribeiro000@gmail.com>
2025-03-13 21:49:43 -0300

    Setting new themes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,7 +5,7 @@ return {
         lazy = true,
         opts = {
             style = "night",
-            transparent = false,
+            transparent = true,
         },
     },
 
@@ -13,15 +13,37 @@ return {
         "tiagovla/tokyodark.nvim",
         lazy = true,
         opts = {
+            transparent_background = true,
             -- custom options here
         },
+    },
+
+    {
+        "zootedb0t/citruszest.nvim",
+        lazy = false, -- Load the plugin immediately
+        priority = 1000, -- High priority to ensure it loads early
+        config = function()
+            -- Configuration for citruszest.nvim
+            require("citruszest").setup({
+                option = {
+                    transparent = true, -- Enable/Disable transparency
+                    bold = false,
+                    italic = true,
+                },
+                -- Override default highlight styles here
+                style = {
+                    -- Example: Change Constant foreground color and make it bold
+                    Constant = { fg = "#FFFFFF", bold = true },
+                },
+            })
+        end,
     },
 
     -- Configura o LazyVim para carregar o tema
     {
         "LazyVim/LazyVim",
         opts = {
-            colorscheme = "tokyodark", -- Aplica o tema TokyoNight
+            colorscheme = "citruszest",
         },
     },
 }
