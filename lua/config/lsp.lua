fa20140bcef371ab267f04544fc4ae7a21231537
guilherme <devguilhermeribeiro000@gmail.com>
2025-03-29 20:33:26 -0300

    ...

---
+++ lua/config/lsp.lua
@@ -0,0 +1,7 @@
+return {
+    servers = {
+        emmet_ls = {
+            filetypes = { "html", "php", "blade" }, -- Adicione outros tipos de arquivo se necessário
+        },
+    },
+}
--- lua/plugins/php.lua
+++ lua/plugins/php.lua
@@ -1,44 +1,8 @@
 return {
-    -- Instalar e configurar o LSP para PHP (Intelephense)
     {
-        "neovim/nvim-lspconfig", -- Configuração dos LSPs no Neovim
-        opts = function()
-            require("lspconfig").intelephense.setup({
-                settings = {
-                    intelephense = {
-                        files = {
-                            associations = { "*.php" }, -- Associar o PHP aos arquivos .php
-                        },
-                        diagnostics = {
-                            suppress = { "missingFileDocComment" },
-                        },
-                    },
-                },
-            })
-        end,
-    },
-
-    -- Instalar o plugin de autocompletar
-    {
-        "hrsh7th/nvim-cmp", -- Autocompletar
-        dependencies = {
-            "hrsh7th/cmp-nvim-lsp", -- Integração do nvim-cmp com LSPs
-        },
-        opts = function()
-            local cmp = require("cmp")
-            cmp.setup({
-                sources = {
-                    { name = "nvim_lsp" }, -- Usar o LSP para autocompletar
-                },
-            })
-        end,
-    },
-
-    -- Instalar o LSP para PHP (Intelephense)
-    {
-        "williamboman/mason.nvim", -- Gerenciador de LSPs
+        "nvim-treesitter/nvim-treesitter",
         opts = {
-            ensure_installed = { "intelephense" }, -- Garantir que o Intelephense seja instalado
+            ensure_installed = { "php" },
         },
     },
 }
